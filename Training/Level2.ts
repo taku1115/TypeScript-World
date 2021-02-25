cd6ede13ea99126477e2941f3748cccb94bf6aee
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-02-25 22:03:01 +0900

    🎉 Training 2-4

--- Training/Level2.ts
+++ Training/Level2.ts
@@ -85,3 +85,42 @@ declare function addEventListener(
   options?: boolean | AddEventListenerOptionsObject
 ): void;
 
+// 4. 文字列型のプロパティidを足してできる新しいオブジェクトを返す関数に型をつける
+function giveId(obj) {
+  const id = "本当はランダムがいいけどここではただの文字列";
+  return {
+    ...obj,
+    id
+  };
+}
+
+// 使用例
+const obj1: {
+  id: string;
+  foo: number;
+} = giveId({ foo: 123 });
+const obj2: {
+  id: string;
+  num: number;
+  hoge: boolean;
+} = giveId({
+  num: 0,
+  hoge: true
+});
+
+// エラー例
+const obj3: {
+  id: string;
+  piyo: string;
+} = giveId({
+  foo: "bar"
+});
+
+// 解答: 新しいプロパティを増やしたい場合の典型的な方法
+function giveId<T>(obj: T): T & { id: string } {
+  const id = "本当はランダムがいいけどここではただの文字列";
+  return {
+    ...obj,
+    id
+  };
+}
