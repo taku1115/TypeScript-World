7d554b034bfc410413ab9d1e1da6ebc451a598af
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-02-25 21:30:26 +0900

    🎉 Training 2-1

---
+++ Training/Level2.ts
@@ -0,0 +1,29 @@
+// 1. ジェネリクス：myFilterに型アノテーションをつける
+function myFilter(arr, predicate) {
+  const result = [];
+  for (const elm of arr) {
+    if (predicate(elm)) {
+      result.push(elm);
+    }
+  }
+  return result;
+}
+
+// 使用例
+const res = myFilter([1, 2, 3, 4, 5], num => num % 2 === 0);
+const res2 = myFilter(['foo', 'hoge', 'bar'], str => str.length >= 4);
+
+// エラー例
+myFilter([1, 2, 3, 4, 5], str => str.length >= 4);
+
+// 解答 : 型引数Tを追加する
+function myFilter<T>(arr: T[], predicate: (elm: T) => boolean): T[] {
+  const result = [];
+  for (const elm of arr) {
+    if (predicate(elm)) {
+      result.push(elm);
+    }
+  }
+  return result;
+}
+
