43db4e5b718249fff83e6f0a5cab5708e573cb34
taku1115 <59477643+taku1115@users.noreply.github.com>
2021-02-25 21:56:28 +0900

    🎉 Training 2-3

--- Training/Level2.ts
+++ Training/Level2.ts
@@ -50,3 +50,38 @@ getSpeed("veryfast");
 // 解答: リテラル型を定義する
 type Speed = 'slow' | 'medium' | 'fast';
 
+// 3. 関数addEventListenerをdeclareを用いて宣言する
+
+// 2つまたは3つの引数を受け取る関数で、返り値なし
+// 1つ目の引数は文字列、2つ目の引数は関数、3つ目の引数は省略可能であり、真偽値またはオブジェクトを渡すことができる。
+// オブジェクトに存在可能なプロパティはcapture, once, passiveの3つで、全て真偽値であり、省略可能。
+
+// 使用例
+addEventListener("foobar", () => {});
+addEventListener("event", () => {}, true);
+addEventListener("event2", () => {}, {});
+addEventListener("event3", () => {}, {
+  capture: true,
+  once: false
+});
+
+// エラー例
+addEventListener("foobar", () => {}, "string");
+addEventListener("hoge", () => {}, {
+  capture: true,
+  once: false,
+  excess: true
+});
+
+// 解答
+type AddEventListenerOptionsObject = {
+  capture?: boolean;
+  once?: boolean;
+  passive?: boolean;
+}
+declare function addEventListener(
+  type: string,
+  handler: () => void,
+  options?: boolean | AddEventListenerOptionsObject
+): void;
+
